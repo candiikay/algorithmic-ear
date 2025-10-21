8cf50be9212aa84223d481f5b2df606deb26e092
Candace Stewart <candace.keenya@gmail.com>
2025-10-21 18:33:34 -0400

    Fix CORS headers to resolve OpaqueResponseBlocking error

--- api/token.js
+++ api/token.js
@@ -5,12 +5,13 @@ export default async function handler(req, res) {
   
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, HEAD')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+  res.setHeader('Access-Control-Max-Age', '86400')
 
   if (req.method === 'OPTIONS') {
     console.log('Handling OPTIONS request')
-    res.status(200).end()
+    res.status(200).json({ message: 'CORS preflight successful' })
     return
   }
 
